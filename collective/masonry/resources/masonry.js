e3e27af0f3a59e8360044e4f46e0b4c8e36ac59f
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-12-28 19:51:46 +0100

    fix jserror: Unexpected token typeof

--- collective/masonry/resources/masonry.js
+++ collective/masonry/resources/masonry.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  if typeof(masonry_configuration) != "undefined"{
+  if (typeof(masonry_configuration) != "undefined"){
     var $portletsContainer = $('.portlets-masonry');
     $portletsContainer.imagesLoaded( function() {
       $portletsContainer.masonry(masonry_configuration);
